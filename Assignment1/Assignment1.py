7d363dff7e74ea04c64bf7bc6684411623c3c717
Friðrik Tryggvi <fridrikr18@ru.is>
2023-11-27 20:04:07 +0000

    fixed myMethod with added pixel analysis and modifications of fps calculations

--- Assignment1/Assignment1.py
+++ Assignment1/Assignment1.py
@@ -1,15 +1,15 @@
 import cv2
+import numpy as np
 
 cap = cv2.VideoCapture(0)
 # set timer to measure fps
 def builtIn():
-    timer = cv2.getTickCount()
     while(True):
+        timer = cv2.getTickCount()  
         #### FPS counter 
         ret, frame = cap.read()
         # calculate fps
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-        latency = (cv2.getTickCount() - timer) / cv2.getTickFrequency()*1000
+        # latency = (cv2.getTickCount() - timer) / cv2.getTickFrequency()*1000
 
 
         original = frame.copy()
@@ -18,7 +18,6 @@ def builtIn():
         frame = cv2.GaussianBlur(frame, (blur, blur), 0)
 
         # update timer
-        timer = cv2.getTickCount()
 
         ### find the brightest spot 
         # find the brightest spot in the color image
@@ -41,6 +40,7 @@ def builtIn():
         #find the reddest spot in the R channel
         (minValRed, maxValRed, minLocRed, maxLocRed) = cv2.minMaxLoc(ultraRed)
 
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
         # draw a circle around the maximum red value
         cv2.circle(original, maxLocRed, 5, (0, 0, 255), 2)
 
@@ -48,7 +48,6 @@ def builtIn():
         cv2.circle(original, maxLoc, 5, (255, 0, 0), 2)
 
         #cv2.circle(ultraRed, maxLocRed, 5, (255, 0, 0), 2)
-
         # put fps on the frame
         cv2.putText(original, "FPS: " + str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
         # put the maximum pixel value on the frame
@@ -56,7 +55,7 @@ def builtIn():
 
         cv2.putText(original, "Max Red: " + str(maxValRed), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-        cv2.putText(original, "Latency: " + str(latency), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        # cv2.putText(original, "Latency: " + str(latency), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         cv2.imshow('hsv',ultraRed)
         #print("FPS = ", fps)
@@ -70,81 +69,55 @@ def builtIn():
     cv2.destroyAllWindows()
 
 def myMethod():
-    timer = cv2.getTickCount()
     while(True):
-        #### FPS counter 
+        timer = cv2.getTickCount()
         ret, frame = cap.read()
-        # calculate fps
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
-        #calculate latency
-        latency = (cv2.getTickCount() - timer) / cv2.getTickFrequency()*1000
 
         original = frame.copy()
-        blur = 15
-        #blur frame 
-        frame = cv2.GaussianBlur(frame, (blur, blur), 0)
+        frame = frame.astype(np.float32) 
+        # Initialize variables for grayscale, brightest spot, and reddest spot
+      
 
-        # update timer
-        timer = cv2.getTickCount()
-
-        ### find the brightest spot 
-        # find the brightest spot in the color image
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-
-        # Split the channels: Hue, Saturation, and Value
-        h, s, v = cv2.split(hsv)
-
-        # Split the channels: Red, Green, and Blue
-        r, g, b = cv2.split(rgb)
-
-        # Subtract the green and blue channels to isolate red
-        ultraRed = cv2.subtract(r, g)
-        ultraRed = cv2.subtract(ultraRed, b)
-
-        # Find the brightest spot in the Value channel
         maxVal = 0
-        maxLoc = (0,0)
-        for i in range(len(v)):
-            for j in range(len(v[i])):
-                if v[i][j] > maxVal:
-                    maxVal = v[i][j]
-                    maxLoc = (i,j)
-
-
-        #find the reddest spot in the R channel
-        #loop through the red channel and find the brightest spot
+        maxLoc = (0, 0)
         maxValRed = 0
-        maxLocRed = (0,0)
-        for i in range(len(ultraRed)):
-            for j in range(len(ultraRed[i])):
-                if ultraRed[i][j] > maxValRed:
-                    maxValRed = ultraRed[i][j]
-                    maxLocRed = (i,j)
+        maxLocRed = (0, 0)
 
-        #flip the coordinates
-        maxLoc = (maxLoc[1], maxLoc[0])
-        maxLocRed = (maxLocRed[1], maxLocRed[0])
+        # Iterate over each pixel for grayscale conversion and finding spots
+        for i in range(frame.shape[0]):
+            for j in range(frame.shape[1]):
+                B, G, R = frame[i, j]
+
+                # Grayscale conversion using luminance formula
+                gray_value = 0.299 * R + 0.587 * G + 0.114 * B
+                
+                # Update brightest spot
+                if gray_value > maxVal:
+                    maxVal = gray_value
+                    maxLoc = (j, i)  # Swap to (x, y)
+
+                # Calculate red value for reddest spot
+                red_value = R - (G + B) / 2
+
+                # Update reddest spot
+                if red_value > maxValRed:
+                    maxValRed = red_value
+                    maxLocRed = (j, i)  # Swap to (x, y)
 
 
-        # draw a circle around the maximum red value
-        cv2.circle(original, maxLocRed, 5, (0, 0, 255), 2)
-
-        # draw a circle around the maximum brightness value
-        cv2.circle(original, maxLoc, 5, (255, 0, 0), 2)
-
-        #cv2.circle(ultraRed, maxLocRed, 5, (255, 0, 0), 2)
+        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+        # Drawing and annotations
+        cv2.circle(original, maxLoc, 5, (255, 0, 0), 2)    # Brightest spot
+        cv2.circle(original, maxLocRed, 5, (0, 0, 255), 2) # Reddest spot
 
         # put fps on the frame
-        cv2.putText(original, "FPS: " + str(int(fps)), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
+        cv2.putText(original, "FPS: " + str(fps), (10, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
         # put the maximum pixel value on the frame
         cv2.putText(original, "Max Brigthness: " + str(maxVal), (10, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
         cv2.putText(original, "Max Red: " + str(maxValRed), (10, 90), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
 
-        cv2.putText(original, "Latency: " + str(latency), (10, 120), cv2.FONT_HERSHEY_SIMPLEX, 0.75, (0, 0, 255), 2)
-
-        cv2.imshow('hsv',ultraRed)
+        #cv2.imshow('hsv',ultraRed)
         #print("FPS = ", fps)
 
         cv2.imshow('frame',original)
